4a33d3eae120cb7df30adf2d8b851e8e4282ffd5
Kaic Pierre Silva <kpisilva@haoc.com.br>
2021-08-20 14:26:11 -0300

    Inicio Masterclass

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function home() {
+    return (
+        <div>
+            <h1>Fundamentos de Next.js & React</h1>
+            <h2>Aprendendo esse framework</h2>
+        </div>
+    )
+}
